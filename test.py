ccac90a73998e1e05538fce66ea7fc0f453df639
Johannes Hofmeister <cessor@github.com>
2013-01-16 14:09:58 +0100

    initialized doscolors

---
+++ test.py
@@ -0,0 +1,28 @@
+from doscolors import *
+
+@background(Colors.red)
+def error(text):
+	print text
+
+@foreground(Colors.green)
+def success(text):
+	print text
+
+@colored(Colors.black, Colors.yellow)
+def warning(text):
+	print text
+
+def manual(text):
+	color(Colors.lightcyan, Colors.blue)
+	print 'Text'
+	reset()
+
+def test():
+	success('Success')
+	error('Error')
+	warning('Warning')
+	manual('Manual')
+	
+if __name__ == '__main__':
+	test()
+	"Use the decorators to access nice colors."
