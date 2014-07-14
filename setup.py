1f0b879c5dfc5acc163bea50b3bc3d5e1cdf9709
Low Kian Seong <kianseong@gmail.com>
2014-07-14 09:19:28 +0800

    Adding more metadata to setup.py and cleaning up some pep8 violations

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='doscolors',
       version='1.0',
+      description='doscolors',
+      url='https://github.com/cessor/doscolors',
       py_modules=['doscolors'],
       )
